fa40eba94dfffacf6f77361a3a1f062aaae74c7b
Peeyush Kumar Singh <91616196+konpeeyush@users.noreply.github.com>
2022-10-13 12:08:41 +0530

    Time: 27 ms (67.75%), Space: 45.2 MB (53.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,30 +1,16 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
+       int max=amount+1 ;
         int[] dp=new int[amount+1] ;
-        Arrays.fill(dp,-1);
-        int ans=helper(coins,amount,dp);
-        if(ans==Integer.MAX_VALUE){
-            return -1 ;
-        }
-        return ans ;
-    }
-    private int helper(int[] coins, int amount,int[]dp){
-        if(amount==0) 
-            return 0 ;
-        if(amount<0)
-            return Integer.MAX_VALUE ;
-        if(dp[amount]!=-1){
-            return dp[amount] ;
-        }
-        
-        int minimum=Integer.MAX_VALUE ;
-        for(int i=0;i<coins.length;i++){
-            int ans=helper(coins,amount-coins[i],dp) ;
-            if(ans!=Integer.MAX_VALUE){
-                minimum=Math.min(minimum,ans+1) ;
+        Arrays.fill(dp,max) ;
+        dp[0]=0 ;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.length;j++){
+                if(coins[j]<=i){
+                    dp[i]=Math.min(dp[i],dp[i-coins[j]]+1) ;
+                }
             }
         }
-        dp[amount]=minimum ;
-        return minimum ;
+        return dp[amount]>amount ? -1 : dp[amount] ;
     }
 }
